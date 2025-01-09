dd5677cb76b9f0d65beae70feaa3c7b841f28a32
jayashree-devi <jayashreedevi1211@gmail.com>
2025-01-09 19:43:53 +0100

    Update wait_for_db.sh

--- wait_for_db.sh
+++ wait_for_db.sh
@@ -1,12 +1,14 @@
-#!binsh
-set -e
+#!/bin/bash
 
-host=$1
-shift
-
-until mariadb -h $host -u$DB_USER -p$DB_PWD -e SHOW DATABASES;  do
-  echo Waiting for database...
-  sleep 2
+# Wait for MariaDB to be ready
+while ! mysqladmin ping --silent; do
+    echo "Waiting for MariaDB to start..."
+    sleep 2
 done
 
-exec $@
+echo "MariaDB is up and running."
+
+# Create database and user
+mariadb -h $DB_HOST -u $DB_USER -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mariadb -h $DB_HOST -u $DB_USER -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mariadb -h $DB_HOST -u $DB_USER -e "FLUSH PRIVILEGES;"
